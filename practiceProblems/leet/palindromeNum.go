d2f71cc9b37e7d4e433da1be4ef580a9ae54dc07
hanmabaki <hongchris96@hotmail.com>
2022-07-23 20:24:08 -0700

    First attempt palindrome number

---
+++ practiceProblems/leet/palindromeNum.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(isPalindrome(121))
+	fmt.Println(isPalindrome(-121))
+	fmt.Println(isPalindrome(10))
+}
+
+func isPalindrome(x int) bool {
+	if x < 0 {
+		return false
+	}
+
+	digits := []int{}
+	for x > 0 {
+		digit := x % 10
+		digits = append(digits, digit)
+		x /= 10
+	}
+
+	for i, j := 0, len(digits)-1; i < j; i, j = i+1, j-1 {
+		if digits[i] != digits[j] {
+			return false
+		}
+	}
+
+	return true
+}
