f54dd822085329c53cce6ca03673677b2ee14226
hanmabaki <hongchris96@hotmail.com>
2022-07-25 13:53:05 -0700

    Create files for upcoming DSA concepts

---
+++ algorithm/binarySearch.go
@@ -0,0 +1,6 @@
+package main
+
+func main() {
+	// binary search
+	// DFS and BFS
+}
---
+++ algorithm/sorting.go
@@ -0,0 +1,10 @@
+package main
+
+func main() {
+	// Merge sort
+	// Quick sort
+	// Bucket sort
+	// Heap sort
+	// Counting sort
+	// Selection sort
+}
