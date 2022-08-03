be6d4884e9774e712a0eb93269a15d202764b88e
hanmabaki <hongchris96@hotmail.com>
2022-08-03 11:55:11 -0700

    Complete merge k sorted list

---
+++ practiceProblems/leet/mergeKsortedList.go
@@ -0,0 +1,43 @@
+package main
+
+import "fmt"
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func main() {
+	fmt.Println("Merge K Sorted List")
+}
+
+func mergeKLists(lists []*ListNode) *ListNode {
+	head := ListNode{}
+	tracker := &head
+	allNil := false
+	for !allNil {
+		allNil = true
+		currentMinValue := getMinValFromHeads(lists)
+		for i, node := range lists {
+			if node != nil {
+				allNil = false
+				if node.Val == currentMinValue {
+					tracker.Next = node
+					tracker = tracker.Next
+					lists[i] = node.Next
+				}
+			}
+		}
+	}
+	return head.Next
+}
+
+func getMinValFromHeads(lists []*ListNode) int {
+	min := 100000
+	for _, node := range lists {
+		if node != nil && node.Val < min {
+			min = node.Val
+		}
+	}
+	return min
+}
