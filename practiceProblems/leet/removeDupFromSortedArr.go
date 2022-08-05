73784ce914abfd9bb9a473f334b7d83ba0b8d5a4
hanmabaki <hongchris96@hotmail.com>
2022-08-05 11:11:44 -0700

    Optimize code to improve run time

--- practiceProblems/leet/removeDupFromSortedArr.go
+++ practiceProblems/leet/removeDupFromSortedArr.go
@@ -14,11 +14,18 @@ func main() {
 func removeDuplicates(nums []int) int {
 	i := 0
 	for i < len(nums) {
-		if i > 0 && nums[i] == nums[i-1] {
-			nums = append(nums[:i], nums[i+1:]...)
-		} else {
-			i++
+		if i > 0 {
+			firstDup := i - 1
+			lastDup := i
+			for lastDup < len(nums) && nums[firstDup] == nums[lastDup] {
+				lastDup++
+			}
+			nums = append(nums[:firstDup+1], nums[lastDup:]...)
+			if lastDup != i {
+				i--
+			}
 		}
+		i++
 	}
 	return len(nums)
 }
