1cd58ccb72362652b05b6c50349e7db268300b36
hanmabaki <hongchris96@hotmail.com>
2022-08-08 12:30:09 -0700

    Update divide two integer with bit operation solution

--- practiceProblems/leet/divideTwoInt.go
+++ practiceProblems/leet/divideTwoInt.go
@@ -1,20 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
 	fmt.Println(divide(10, 3))
 }
 
 func divide(dividend int, divisor int) int {
+	// updated bit operation solution from https://www.youtube.com/watch?v=htX69j1jf5U
+	if math.MinInt32 == dividend && divisor == -1 {
+		return math.MaxInt32
+	}
 	num1 := ab(dividend)
 	num2 := ab(divisor)
 	count := 0
-	for num1 >= num2 {
-		num1 -= num2
-		count++
+	for num1-num2 >= 0 {
+		x := 0 // 2 to the 0
+		for num1-(num2<<1<<x) >= 0 {
+			x++
+		}
+		count += 1 << x
+		num1 -= num2 << x
 	}
-	if (dividend < 0 && divisor > 0) || (dividend > 0 && divisor < 0) {
+	if (dividend >= 0) == (divisor >= 0) {
 		return -count
 	}
 	return count
