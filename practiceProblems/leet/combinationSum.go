80b149587a4e88a1718c86f8ef639e0dee51791c
hanmabaki <hongchris96@hotmail.com>
2022-08-15 11:47:08 -0700

    Complete combination sum with recursive backtracking

--- practiceProblems/leet/combinationSum.go
+++ practiceProblems/leet/combinationSum.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"sort"
 )
 
 func main() {
@@ -10,58 +9,34 @@ func main() {
 	fmt.Println(combinationSum([]int{100, 200, 4, 12}, 400)) // hitting infinite loop
 }
 
+// DFS structure for [2,3,6,7] target 7:
+// 													[]
+// 							[2]                                           []
+// 			[2,2]                  [2]								[3]				[]
+// 	[2,2,2]       [2,2,3]		[2,3]		 [2]			  [3,3]		[3]		   [6]		[]
+// [2,2,2,2]				[2,3,3] [2,3,6]		[2,6]	   [3,3,3]		 [3,6]	  [6,6]			[7]
+
 func combinationSum(candidates []int, target int) [][]int {
-	if target == 0 {
-		return [][]int{[]int{}}
-	}
-
-	sort.Ints(candidates)
-	allCombs := [][]int{}
-	for i := len(candidates) - 1; i >= 0; i-- {
-		if candidates[i] <= target {
-			remaining := target - candidates[i]
-			remainingComb := combinationSum(candidates, remaining)
-			for _, comb := range remainingComb {
-				candidateComb := append([]int{candidates[i]}, comb...)
-				allCombs = append(allCombs, candidateComb)
-			}
-		}
-	}
-
-	fmt.Println(allCombs)
-
-	checked := [][]int{}
-	for i := 0; i < len(allCombs); i++ {
-		dupFound := false
-		for _, checkedComb := range checked {
-			if combDup(checkedComb, allCombs[i]) {
-				dupFound = true
-				break
-			}
-		}
-		if dupFound {
-			copy(allCombs[i:], allCombs[i+1:])
-			allCombs = allCombs[:len(allCombs)-1]
-			i--
-		} else {
-			checked = append(checked, allCombs[i])
-		}
-	}
-	return allCombs
+	answer := [][]int{}
+	// need to pass in slice by reference
+	traverseCombs(candidates, []int{}, &answer, 0, target) // O(2^target): deepest dfs layer is the target
+	return answer
 }
 
-func combDup(arr1, arr2 []int) bool {
-	if len(arr1) != len(arr2) {
-		return false
+func traverseCombs(candidates, current []int, answer *[][]int, total, target int) {
+	if total == target {
+		comb := append([]int{}, current...)
+		*answer = append(*answer, comb)
+		return
+	} else if len(candidates) <= 0 || total > target {
+		return
 	}
-	arr1 = append([]int{}, arr1...)
-	arr2 = append([]int{}, arr2...)
-	sort.Ints(arr1)
-	sort.Ints(arr2)
-	for i := 0; i < len(arr1); i++ {
-		if arr1[i] != arr2[i] {
-			return false
-		}
-	}
-	return true
+
+	// includes the first candidate
+	current = append(current, candidates[0])
+	traverseCombs(candidates, current, answer, total+candidates[0], target)
+
+	// no longer includes the first candidate
+	current = append(current[:len(current)-1], current[len(current):]...)
+	traverseCombs(candidates[1:], current, answer, total, target)
 }
