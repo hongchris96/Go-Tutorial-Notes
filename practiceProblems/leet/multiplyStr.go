19120ca908d9ecc98d368aed726aaa31d2b148f7
hanmabaki <hongchris96@hotmail.com>
2022-08-19 10:55:29 -0700

    Complete multiply string

--- practiceProblems/leet/multiplyStr.go
+++ practiceProblems/leet/multiplyStr.go
@@ -7,32 +7,54 @@ func main() {
 }
 
 func multiply(num1 string, num2 string) string {
-	int1 := 0
-	int2 := 0
-	for i, dig := range num1 {
-		int1 += tenPow(len(num1)-1-i) * int(dig-48)
-	}
-	for i, dig := range num2 {
-		int2 += tenPow(len(num2)-1-i) * int(dig-48)
-	}
-	prod := int1 * int2
-	fmt.Println(prod)
-	if prod == 0 {
+	if num1 == "0" || num2 == "0" {
 		return "0"
 	}
-	answer := ""
-	for prod > 0 {
-		dig := prod % 10
-		answer = string(dig+48) + answer
-		prod /= 10
-	}
-	return answer
-}
 
-func tenPow(pow int) int {
-	answer := 1
-	for i := 0; i < pow; i++ {
-		answer *= 10
+	answer := []int{}
+	// performing math operation one digit at a time right to left
+	carryOver := 0
+	for i := len(num1) - 1; i > -1; i-- {
+		rune1 := num1[i]
+		carryOver = 0
+		for j := len(num2) - 1; j > -1; j-- {
+			rune2 := num2[j]
+			// getting product of current two digits
+			digProd := int(rune2-48)*int(rune1-48) + carryOver
+			// adding product to the right place in the answer
+			answerDigPosition := len(num1) + len(num2) - j - i - 2
+			// either adding to existing slot or append to a new slot
+			if answerDigPosition < len(answer) {
+				digProd += answer[answerDigPosition]
+				// slot only holds single digit integers
+				// update carryover to next operation
+				if digProd > 9 {
+					answer[answerDigPosition] = digProd % 10
+					carryOver = digProd / 10
+				} else {
+					answer[answerDigPosition] = digProd
+					carryOver = 0
+				}
+			} else {
+				if digProd > 9 {
+					answer = append(answer, digProd%10)
+					carryOver = digProd / 10
+				} else {
+					answer = append(answer, digProd)
+					carryOver = 0
+				}
+			}
+		}
+		// append a new slot for carryover after each inner loop
+		if carryOver > 0 {
+			answer = append(answer, carryOver)
+		}
 	}
-	return answer
+	// convert slice of int to string
+	stringAnswer := ""
+	for _, num := range answer {
+		stringAnswer = string(num+48) + stringAnswer
+	}
+	return stringAnswer
+
 }
