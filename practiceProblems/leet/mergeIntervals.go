79b3c47fe5eea91376a62023529d5de9a7409855
hanmabaki <hongchris96@hotmail.com>
2022-08-29 15:37:18 -0700

    Complete merge intervals

--- practiceProblems/leet/mergeIntervals.go
+++ practiceProblems/leet/mergeIntervals.go
@@ -3,13 +3,30 @@ package main
 import (
 	"fmt"
 	"math"
+	"sort"
 )
 
+type customSlice [][]int
+
 func main() {
-	fmt.Println()
+	fmt.Println(merge([][]int{{2, 3}, {4, 5}, {6, 7}, {8, 9}, {1, 10}})) // [[1, 10]]
+	fmt.Println(merge([][]int{{1, 3}, {2, 6}, {8, 10}, {15, 18}}))       // [[1,6],[8,10],[15,18]]
+	fmt.Println(merge([][]int{{1, 4}, {4, 5}}))                          // [[1, 5]]
+}
+
+func (s customSlice) Len() int {
+	return len(s)
+}
+func (s customSlice) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
+}
+
+func (s customSlice) Less(i, j int) bool {
+	return s[i][0] < s[j][0]
 }
 
 func merge(intervals [][]int) [][]int {
+	sort.Sort(customSlice(intervals))
 	for i := 1; i < len(intervals); i++ {
 		prevLower := float64(intervals[i-1][0])
 		prevUpper := float64(intervals[i-1][1])
