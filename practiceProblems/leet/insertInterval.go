f5d960a61f4c3f03b61cd3158f736ade9ac0910e
hanmabaki <hongchris96@hotmail.com>
2022-08-30 12:00:04 -0700

    Optimize code for insert interval

--- practiceProblems/leet/insertInterval.go
+++ practiceProblems/leet/insertInterval.go
@@ -7,47 +7,21 @@ func main() {
 }
 
 func insert(intervals [][]int, newInterval []int) [][]int {
-	merged := false
+	results := make([][]int, 0)
 	for i := 0; i < len(intervals); i++ {
-		if !merged {
-			if mergable(intervals[i], newInterval) {
-				intervals[i][0] = min(intervals[i][0], newInterval[0])
-				intervals[i][1] = max(intervals[i][1], newInterval[1])
-				merged = true
-			} else if insertable(intervals[i][0], newInterval) {
-				intervals = append(intervals[:i+1], intervals[i:]...)
-				intervals[i] = newInterval
-				merged = true
-			}
-			continue
-		}
-		if merged && intervals[i][0] <= intervals[i-1][1] {
-			intervals[i-1][1] = max(intervals[i-1][1], intervals[i][1])
-			intervals = append(intervals[:i], intervals[i+1:]...)
-			i--
+		if newInterval[1] < intervals[i][0] {
+			results = append(results, newInterval)
+			results = append(results, intervals[i:]...)
+			return results
+		} else if newInterval[0] > intervals[i][1] {
+			results = append(results, intervals[i])
+		} else {
+			newInterval[0] = min(newInterval[0], intervals[i][0])
+			newInterval[1] = max(newInterval[1], intervals[i][1])
 		}
 	}
-	if !merged {
-		intervals = append(intervals, newInterval)
-	}
-	return intervals
-}
-
-func insertable(lower int, interval []int) bool {
-	if interval[1] < lower {
-		return true
-	}
-	return false
-}
-
-func mergable(interval1, interval2 []int) bool {
-	if interval2[0] <= interval1[1] && interval2[1] >= interval1[1] {
-		return true
-	} else if interval1[0] <= interval2[1] && interval1[1] >= interval2[1] {
-		return true
-	} else {
-		return false
-	}
+	results = append(results, newInterval)
+	return results
 }
 
 func max(int1, int2 int) int {
