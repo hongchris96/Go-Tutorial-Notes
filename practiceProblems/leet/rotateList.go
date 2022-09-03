eb19c786f62dbf7e25456890c643d3fc04724d7d
hanmabaki <hongchris96@hotmail.com>
2022-09-02 18:50:57 -0700

    Refactor rotate list to use less memory

--- practiceProblems/leet/rotateList.go
+++ practiceProblems/leet/rotateList.go
@@ -17,14 +17,18 @@ func rotateRight(head *ListNode, k int) *ListNode {
 		return nil
 	}
 
-	nodes := map[int]*ListNode{}
+	begin := ListNode{}
+	end := ListNode{}
+	begin.Next = head
 
-	// get length and store nodes
+	// get length and save pointer to last node
 	tracker := head
 	length := 0
 	for tracker != nil {
-		nodes[length] = tracker
 		length++
+		if tracker.Next == nil {
+			end.Next = tracker
+		}
 		tracker = tracker.Next
 	}
 
@@ -33,11 +37,46 @@ func rotateRight(head *ListNode, k int) *ListNode {
 		return head
 	}
 
-	// performing restructure
+	// find point of pivot
 	cutFrom := length - lastNodes - 1
-	answer := nodes[cutFrom+1]
-	nodes[cutFrom].Next = nil
-	nodes[length-1].Next = head
+	tracker = head
+	for i := 0; i < cutFrom; i++ {
+		tracker = tracker.Next
+	}
+	// performing restructure
+	newTail := tracker
+	newHead := tracker.Next
+	newTail.Next = nil
+	end.Next.Next = head
+	return newHead
 
-	return answer
+	// ----------------------------------------------------
+
+	// if head == nil {
+	// 	return nil
+	// }
+
+	// nodes := map[int]*ListNode{}
+
+	// // get length and store nodes
+	// tracker := head
+	// length := 0
+	// for tracker != nil {
+	// 	nodes[length] = tracker
+	// 	length++
+	// 	tracker = tracker.Next
+	// }
+
+	// lastNodes := k % length
+	// if lastNodes == 0 {
+	// 	return head
+	// }
+
+	// // performing restructure
+	// cutFrom := length - lastNodes - 1
+	// answer := nodes[cutFrom+1]
+	// nodes[cutFrom].Next = nil
+	// nodes[length-1].Next = head
+
+	// return answer
 }
