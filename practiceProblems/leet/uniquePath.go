7736a2b196b721edd6fb2f479d9bea8bcd380ff5
hanmabaki <hongchris96@hotmail.com>
2022-09-03 14:17:01 -0700

    Add cache to pass time limit

--- practiceProblems/leet/uniquePath.go
+++ practiceProblems/leet/uniquePath.go
@@ -7,19 +7,24 @@ func main() {
 }
 
 func uniquePaths(m int, n int) int {
-	return traverse(0, 0, m-1, n-1)
+	cached := map[[2]int]int{}
+	return traverse(0, 0, m-1, n-1, cached)
 }
 
-func traverse(row, col, destRow, destCol int) int {
+func traverse(row, col, destRow, destCol int, cached map[[2]int]int) int {
 	if row == destRow && col == destCol {
 		return 1
 	}
+	if val, ok := cached[[2]int{row, col}]; ok {
+		return val
+	}
 	count := 0
 	if row < destRow {
-		count += traverse(row+1, col, destRow, destCol)
+		count += traverse(row+1, col, destRow, destCol, cached)
 	}
 	if col < destCol {
-		count += traverse(row, col+1, destRow, destCol)
+		count += traverse(row, col+1, destRow, destCol, cached)
 	}
+	cached[[2]int{row, col}] = count
 	return count
 }
